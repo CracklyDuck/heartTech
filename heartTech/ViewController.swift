1e7c967746e12c196c52eae3d87454808bc6b0c2
Nora Villarreal <a01037004@itesm.mx>
2022-09-19 15:57:41 -0500

    Menus

--- heartTech/ViewController.swift
+++ heartTech/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgIniciarSesion: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imgIniciarSesion.layer.masksToBounds = true
+           imgIniciarSesion.layer.cornerRadius = imgIniciarSesion.bounds.width / 2
         // Do any additional setup after loading the view.
     }
 
