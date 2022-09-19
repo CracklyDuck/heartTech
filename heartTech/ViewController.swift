8778215fadb8b0abe945e6eeb89b631e5b4d9ee0
carminalp <74677482+carminalp@users.noreply.github.com>
2022-09-19 13:23:06 -0500

    Initial Commit

---
+++ heartTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  heartTech
+//
+//  Created by Alumno on 19/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
