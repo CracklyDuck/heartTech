2148d624ea04ad50c58f02365757fb88d2f879b0
carminalp <trejo.fabian@hotmail.com>
2022-09-19 15:49:04 -0500

    Firebase 1.0
    Inclusión de Firebase

---
+++ Aux.swift
@@ -0,0 +1,29 @@
+//
+//  Aux.swift
+//  heartTech
+//
+//  Created by Alumno on 19/09/22.
+//
+
+import Foundation
+/*
+import UIKit
+import FirebaseCore
+content_copy
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+content_copy
+
+    return true
+  }
+}
+*/
